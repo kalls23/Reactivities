b48b32af8fa2019b27819f126b6ae24b7bb26c13
kalls23 <56848853+kalls23@users.noreply.github.com>
2019-12-23 12:18:53 -0500

    Sec 3 complete

--- API/Startup.cs
+++ API/Startup.cs
@@ -22,6 +22,12 @@ namespace API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<DataContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddCors( opt =>
+            {
+                opt.AddPolicy("CorsPolicy", policy => {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3001");
+                });
+            });
             services.AddControllers();
         }
 
@@ -38,6 +44,8 @@ namespace API
             app.UseRouting();
 
             app.UseAuthorization();
+
+            app.UseCors("CorsPolicy");
 
             app.UseEndpoints(endpoints =>
             {
